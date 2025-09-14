bcb01cb5065217b45dabd4484dba6a3bd364d84e
Jimmy Phan <phanjimmy02@gmail.com>
2025-09-14 15:51:32 -0400

    Guess target number

--- Numberguess.py
+++ Numberguess.py
@@ -1,5 +1,16 @@
 import random
 
-x = random.randint(1, 100)
-guess = int(input('Guess number from 1 and 100: '))
-print(guess)
+target_number = random.randint(1, 100)
+while True:
+    try:
+        guess = int(input('Guess number from 1 and 100: '))
+        if guess < target_number:
+            print('Too low!')
+        elif guess > target_number:
+            print('Too high!')
+        else:
+            print('Congratulations!')
+            break
+    except ValueError: 
+        print('Enter a valid number')
+
